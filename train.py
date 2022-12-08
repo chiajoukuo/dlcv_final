14ec14684160c555ba66e1435aee2c48bbaeb8f2
chiajoukuo <b05901166@ntu.edu.tw>
2022-12-08 04:11:32 -0500

    done1

--- train.py
+++ train.py
@@ -42,7 +42,6 @@ for epoch in range(1, EPOCHS+1):
 
     loss_sum += loss.item()
     print('{} / {}'.format(i, numsss), '\r')
-    break
   print('Epoch {} loss {}'.format(epoch, loss))
   torch.save({'epoch': EPOCHS, 'state_dict': model.state_dict(), 'optimizer' : optimizer.state_dict()}, './models/model_{}.pth.tar'.format(epoch))
 
